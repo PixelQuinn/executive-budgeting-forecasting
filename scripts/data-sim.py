6f4130d718d92bd3c24335bf9972db9df2dc7913
Quinn Reams <quinn.reams@icloud.com>
2025-08-10 14:02:19 -0400

    doc: period range defined

--- scripts/data-sim.py
+++ scripts/data-sim.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 # -----------------------------------------
 # Executive Budgeting + Forecasting Project
 # Step 1: Config and monthly index setup
@@ -14,3 +16,12 @@ DEPARTMENTS = ["Finance", "HR", "Sales", "Operations", "Marketing"]
 
 OUTPUT_DIR = "../data" 
 KEEP_QA_COMPONENTS = True # Change to keep intermediate QA columns(True) or not(False) for debugging
+
+# --- Index ---
+MONTHS = pd.period_range(start = START_MONTH, end = END_MONTH, freq = "M")
+
+# Test for period_range
+print(len(MONTHS))      # total number of months
+print(MONTHS[:3])       # first 3 months
+print(MONTHS[-3:])      # last 3 months
+print(MONTHS.dtype)     # should be period[M]
